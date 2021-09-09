454c59167f730916fa476567fcb4016decddf052
Julio Cárdenas <julio.cardenas7606@alumnos.udg.mx>
2021-09-09 00:43:08 -0500

    Creación de proyecto

---
+++ src/views/Home.vue
@@ -0,0 +1,68 @@
+<template>
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-title>home</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    
+    <ion-content :fullscreen="true" :style="{backgroundImage:'url(https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/David_Motta_Soares_ballet_dancer.jpg/640px-David_Motta_Soares_ballet_dancer.jpg)'}">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Bienvenido</ion-title>
+        </ion-toolbar>
+      </ion-header>
+    
+      <div id="container">
+        <p>Iniciar sesión</p>
+        <p>Registrarme</p>
+      </div>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Home',
+  components: {
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonTitle,
+    IonToolbar
+  }
+});
+</script>
+
+<style scoped>
+#container {
+  text-align: center;
+  
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%);
+}
+
+#container strong {
+  font-size: 20px;
+  line-height: 26px;
+}
+
+#container p {
+  font-size: 16px;
+  line-height: 22px;
+  
+  color: #8c8c8c;
+  
+  margin: 0;
+}
+
+#container a {
+  text-decoration: none;
+}
+</style>
